a0c18e9a02b37305240dd9b98956be818531c353
Adrian Suarez <asuarez9382@gmail.com>
2024-04-01 16:02:12 -0700

    complete lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,22 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+
+    def delete(self, value):
+        del self.dictionary[value]
+        return self
+
+    def size(self):
+        return len(self.dictionary)
